609c7ce01ae74b023ce7c20b6b6be339d8f660b7
Kyaw Hlaing Soe <kyawhlaingsoe572@gmail.com>
2026-06-30 22:23:01 +0630

    first commit

---
+++ src/test/java/com/example/eventticketplatform/EventTicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventticketplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
