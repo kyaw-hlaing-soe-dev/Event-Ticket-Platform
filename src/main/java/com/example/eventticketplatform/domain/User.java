0d35791d259e9461d207137b534d551f303fb14c
Kyaw Hlaing Soe <kyawhlaingsoe572@gmail.com>
2026-07-07 21:14:32 +0630

    add User entity with fields for id, name, and email

---
+++ src/main/java/com/example/eventticketplatform/domain/User.java
@@ -0,0 +1,26 @@
+package com.example.eventticketplatform.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "name" , nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+}
